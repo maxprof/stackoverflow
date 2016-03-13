a344a34e2f72ebd49525e2d0903afe01e7e4a0e0
Max Cherevatov <maxprofkom@gmail.com>
2016-03-13 19:13:08 +0200

    new settings for reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,3 +33,13 @@ Rails.application.configure do
   # }
   # config.action_mailer.raise_delivery_errors = true
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
