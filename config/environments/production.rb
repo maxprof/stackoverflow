d5d1ca5c1890b040011e03f654eb55ce55e5a1f8
Max Cherevatov <maxprofkom@gmail.com>
2016-03-13 14:28:47 +0200

    Try customize devise password  confirmation4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.delivery_method = :sendmail 
   config.action_mailer.default_url_options = { :host => 'cinema-booking.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
         :address        => 'smtp.gmail.com',
