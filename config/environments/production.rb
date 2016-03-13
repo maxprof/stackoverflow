00ee1bf00229b3a95f4b15c17477463958422e84
Max Cherevatov <maxprofkom@gmail.com>
2016-03-13 17:49:25 +0200

    Try customize devise password  confirmation6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'cinema-booking.herokuapp.com' }
   config.action_mailer.smtp_settings = {
         :address        => 'smtp.gmail.com',
-        :domain         => 'cinema-booking.herokuapp.com',
+        :domain         => 'gmail.com',
         :port           => 587,
         :user_name      => ENV['maxprofkom@gmail.com'],
         :password       => ENV['yiykjrgpxhvvwfmy'],
