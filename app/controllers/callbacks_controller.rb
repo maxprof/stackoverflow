c8d5c9bcdd158bde50f9b9ef52fa1ed875f6ac9d
Max Cherevatov <maxprofkom@gmail.com>
2016-03-14 16:06:51 +0200

    Config flash messages v1

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,11 +2,13 @@ class CallbacksController < Devise::OmniauthCallbacksController
 	
     def vkontakte
         @user = User.from_omniauth(request.env["omniauth.auth"])
+  		flash[:success] = "You are succsessfuly logged in"
         sign_in_and_redirect @user
     end
 
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
+  		flash[:success] = "You are succsessfuly logged in"
         sign_in_and_redirect @user
     end
     
