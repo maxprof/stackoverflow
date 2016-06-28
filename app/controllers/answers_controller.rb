0641c7f7c5374234b1f075ab611aaa7c287166b2
Max Cherevatov <maxprofkom@gmail.com>
2016-06-28 18:03:34 +0300

    add ajax for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -30,9 +30,12 @@ class AnswersController < ApplicationController
       if @answer.save
         format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
         format.json { render :show, status: :created, location: @answer }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @answer.errors, status: :unprocessable_entity }
+        format.js
+
       end
     end
   end
