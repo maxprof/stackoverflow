6c58243c27eaf88afe7ab432772c7385301df159
Max Cherevatov <maxprofkom@gmail.com>
2016-07-01 20:53:31 +0300

    add admin rules for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   helper_method :get_user_ranking
+  before_action :check_rules, only: [:destroy]
+  before_action :check_admin_rules, only: [:show, :edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to (:back), notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   def update
@@ -21,6 +33,30 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def check_rules
+      if user_signed_in?
+        if current_user.role != "admin" && current_user.id != @user.id
+          flash[:success] = "It's not your profile"
+          redirect_to (:back)
+        end
+      else
+       flash[:success] = "You not signed in"
+       redirect_to root_path
+      end
+    end
+
+    def check_admin_rules
+      if user_signed_in?
+        if current_user.role != "admin"
+          flash[:success] = "You have no rights"
+          redirect_to (:back)
+        end
+      else
+       flash[:success] = "You not signed in"
+       redirect_to (:back)
+      end
+    end
 
     def get_user_ranking(user)
       @user = User.find(user.id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-  has_many :questions
-  has_many :answers
+  has_many :questions, :dependent => :destroy
+  has_many :answers, :dependent => :destroy
 
 
   def self.from_omniauth(auth)
