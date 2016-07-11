68913a9ac218255957d4d3d6804b8bfc9ca3fe73
Max Cherevatov <maxprofkom@gmail.com>
2016-07-11 20:32:54 +0300

    Cancel confirmation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   acts_as_voter
   def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      where(provider: auth.provider, uid: auth.uid). first_or_initialize do |user|
         user.provider = auth.provider
         user.name = auth.info.name
         user.uid = auth.uid
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
+        user.skip_confirmation!
+        user.save
       end
   end
 end
